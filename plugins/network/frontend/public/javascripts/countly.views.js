8c4ba74599b94891679915bb83b1ec76abb09ea6
darian.miao <darian.miao@moca-tech.net>
2018-06-20 13:01:53 +0800

    add link

--- plugins/network/frontend/public/javascripts/countly.views.js
+++ plugins/network/frontend/public/javascripts/countly.views.js
@@ -65,7 +65,7 @@ window.NetworkView = countlyView.extend({
                     //  return countlyCommon.timeString(time/1000);
                     // else return time
                 }, sType:"numeric", "sTitle": jQuery.i18n.map["network.avg-duration"] },
-                    { "mData": "e", sType:"formatted-num", "mRender":function(d) { return "<p class='table-link green'>" +countlyCommon.formatNumber(d)+ "</a>"; }, "sTitle": jQuery.i18n.map["network.http.error-cnts"] }
+                    { "mData": "e", sType:"formatted-num", "mRender":function(d) { return "<p class='table-link green link-class'>" +countlyCommon.formatNumber(d)+ "</a>"; }, "sTitle": jQuery.i18n.map["network.http.error-cnts"] }
             //    { "mData": "s", sType:"formatted-num", "mRender":function(d) { return countlyCommon.formatNumber(d); }, "sTitle": jQuery.i18n.map["network.starts"] },
             //    { "mData": "e", sType:"formatted-num", "mRender":function(d) { return countlyCommon.formatNumber(d); }, "sTitle": jQuery.i18n.map["network.exits"] },
             //    { "mData": "b", sType:"formatted-num", "mRender":function(d) { return countlyCommon.formatNumber(d); }, "sTitle": jQuery.i18n.map["network.bounces"] }
@@ -105,7 +105,7 @@ window.NetworkView = countlyView.extend({
                 },
                 "aoColumns": columns
             }));
-            $('.table-link .green').on("click", function (event){
+            $('.class').on("click", function (event){
                 event.stopPropagation();
                 var id = $(this).attr("id");
                 if(id){
