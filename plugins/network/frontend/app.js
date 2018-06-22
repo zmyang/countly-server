4615a03b3bbf49034614959ee177c2e816d0d721
darian.miao <darian.miao@moca-tech.net>
2018-06-22 15:17:50 +0800

    add md5hash

--- plugins/network/frontend/app.js
+++ plugins/network/frontend/app.js
@@ -1,8 +1,11 @@
 var plugin = {};
 
 (function (plugin) {
+	var crypto = require('crypto');
 	plugin.init = function(app, countlyDb){
-		
+		function md5Hash(str) {
+			return crypto.createHash('md5').update(str + "").digest('hex');
+		}
 	};
 }(plugin));
 
