329a17b4f74e22d4e46f8ee80c80a1712287373f
darian.miao <darian.miao@moca-tech.net>
2018-06-22 14:58:22 +0800

    add md5

--- plugins/network/frontend/public/javascripts/countly.views.js
+++ plugins/network/frontend/public/javascripts/countly.views.js
@@ -111,7 +111,7 @@ window.NetworkView = countlyView.extend({
             }));
             $('.link-class').on("click", function (event){
                 event.stopPropagation();
-                var id =$(this).attr("id");
+                var id =app.md5Hash($(this).attr("id"));
                 if(id){
                     var link = "#/networkerror/" + id ;
                     window.open(link, "_self");
