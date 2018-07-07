e19d65a0b80d0d9a613e04e6914bcbbc1b518a65
darian.miao <darian.miao@moca-tech.net>
2018-07-08 01:15:30 +0800

    add metrics

--- plugins/network/frontend/public/javascripts/countly.views.js
+++ plugins/network/frontend/public/javascripts/countly.views.js
@@ -915,33 +915,30 @@ window.NetworkMetricView = countlyView.extend({
         this.templateData = {
             "page-title":jQuery.i18n.map["network.overview-title"],
             "no-data":jQuery.i18n.map["common.bar.no-data"],
+            "total":dashboard.usage['cr'].total,
             "usage":[
 				{
 					"title":jQuery.i18n.map["network.http.request-cnts"],
-                    // "data":dashboard.usage['cr'],
-                    "data":dashboard,
+					"data":dashboard.usage['cr'],
 					"id":"crash-cr",
                     "help":"crashes.help-total"
 				},
 				{
 					"title":jQuery.i18n.map["network.http.error-cnts"],
-					// "data":dashboard.usage['cru'],
-                    "data":dashboard,
-                    "id":"crash-cru",
+					"data":dashboard.usage['cru'],
+					"id":"crash-cru",
                     "help":"crashes.help-unique"
 				},
 				{
 					"title":jQuery.i18n.map["network.http.response-time"],
-					// "data":dashboard.usage['crnf'],
-                    "data":dashboard,
-                    "id":"crash-crnf",
+					"data":dashboard.usage['crnf'],
+					"id":"crash-crnf",
                     "help":"crashes.help-nonfatal"
 				},
 				{
 					"title":jQuery.i18n.map["network.http.reportbytes"],
-					// "data":dashboard.usage['crf'],
-                    "data":dashboard,
-                    "id":"crash-crf",
+					"data":dashboard.usage['crf'],
+					"id":"crash-crf",
                     "help":"crashes.help-fatal"
 				}/*,
 				{
