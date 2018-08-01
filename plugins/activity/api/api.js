1c4e4a16daac7107c8472dad05cb0d01ebb662f8
darian.miao <darian.miao@moca-tech.net>
2018-08-01 16:23:50 +0800

    test comments

--- plugins/activity/api/api.js
+++ plugins/activity/api/api.js
@@ -287,7 +287,7 @@ plugins.setConfigs("activities", {
                         defaultComment._id = common.crypto.createHash('sha1').update(params.app_id + report._id+JSON.stringify(defaultComment)+"").digest('hex');
                         
                         common.db.collection('app_activitygroups' + params.app_id).findOne({'_id': hash}, function(err, res){
-                                if(res.comments && res.comments.length>0){
+                                if(res != null && res.comments && res.comments.length>0){
                                     //no process
                                 }else{
                                     common.db.collection('app_activitygroups' + params.app_id).update({'_id': hash }, {"$push":{'comments':defaultComment}}, function (err, res){});
